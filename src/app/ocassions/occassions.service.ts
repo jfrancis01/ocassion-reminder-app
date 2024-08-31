2eea8b5c99a8e6bcb2a35051de1b68c710edee3a
jfrancis01 <joelfrancis@rogers.com>
2024-08-31 00:32:32 -0400

    fixed all fields except occasion type

--- src/app/ocassions/occassions.service.ts
+++ src/app/ocassions/occassions.service.ts
@@ -2,10 +2,10 @@ import { Ocassion } from "./ocassion.model";
 export class OccassionsService{
     private occassions: Ocassion[] =[
         new Ocassion(1, 'Joel Francis', 'Birthday', new Date("1985, 10, 19"), false, 'week before'),
-        new Ocassion(2, 'Allie Francis', 'Birthday', new Date("1988, 7, 5"), true, 'day before'),
+        new Ocassion(2, 'Allie Francis', 'Graduation', new Date("1988, 7, 5"), true, 'day before'),
         new Ocassion(3, 'Joel Francis', 'Anniversary', new Date("2017, 8, 5"), false, 'day off'),
-        new Ocassion(4, 'Jayce Francis', 'Birthday', new Date("2020, 11, 23"), false, 'day off'),
-        new Ocassion(5, 'Rosalie Francis', 'Birthday', new Date("2023, 04, 26"), true, 'week before')
+        new Ocassion(4, 'Jayce Francis', 'Confirmation', new Date("2020, 11, 23"), false, 'day off'),
+        new Ocassion(5, 'Rosalie Francis', 'Wedding', new Date("2023, 04, 26"), true, 'week before')
       ];
 
     getOccassions(){
