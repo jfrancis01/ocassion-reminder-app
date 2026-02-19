1eb59213a148d2cee13dc1ad1aa550ec154d8409
jfrancis01 <joelfrancis@rogers.com>
2026-02-18 20:53:31 -0500

    added environment files for dev and production

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    production:true,
+    KEYCLOAK_INIT_URL: 'http://keycloak:8080/',
+    KEYCLOAK_REDIRECT_URI: 'http://localhost:4200/home',
+    AUTHSERVICE_LOGIN_URL:'http://localhost:8009/occassionsreminder/login',
+    HEADER_COMPONENT_REDIRECT_URI: 'http://localhost:4200/welcome',
+    OCCASSIONS_SERVICE_GET_OCCASSIONS_URL: 'http://localhost:8009/occassionsreminder/occassions',
+    OCCASSIONS_SERVICE_GET_ONE_OCCASSION_URL: 'http://localhost:8009/occassionsreminder/occassion',
+    OCCASSIONS_SERVICE_ADD_OCCASSION_URL: 'http://localhost:8009/occassionsreminder/add',
+    OCCASSIONS_SERVICE_EDIT_OCCASSION_URL: 'http://localhost:8009/occassionsreminder/edit',
+    OCCASSIONS_SERVICE_DELETE_OCCASSION_URL: 'http://localhost:8009/occassionsreminder/delete',
+    REGISTER_COMPONENT_REGISTER_URL: 'http://localhost:8009/occassionsreminder/register',
+    UPDATE_COMPONENT_GET_USER_URL: 'http://localhost:8009/occassionsreminder/getUser',
+    UPDATE_COMPONENT_EDIT_USER_URL: 'http://localhost:8009/occassionsreminder/editUser'
+
+};
+
+
