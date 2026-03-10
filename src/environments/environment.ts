581caf5b229bdf333643aaae690001faab6b21c3
jfrancis01 <joelfrancis@rogers.com>
2026-03-09 22:15:16 -0400

    changed the production environment port number for the back end

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,16 @@ export const environment = {
     production:true,
     APPMODULE_KEYCLOAK_INIT_URL: 'http://localhost:8082/',
     APPMODULE_KEYCLOAK_REDIRECT_URI: 'http://localhost:4200/home',
-    AUTHSERVICE_LOGIN_URL:'http://localhost:9001/occassionsreminder/login',
+    AUTHSERVICE_LOGIN_URL:'http://localhost:9000/occassionsreminder/login',
     HEADER_COMPONENT_REDIRECT_URI: 'http://localhost:4200/welcome',
-    OCCASSIONS_SERVICE_GET_OCCASSIONS_URL: 'http://localhost:9001/occassionsreminder/occassions',
-    OCCASSIONS_SERVICE_GET_ONE_OCCASSION_URL: 'http://localhost:9001/occassionsreminder/occassion',
-    OCCASSIONS_SERVICE_ADD_OCCASSION_URL: 'http://localhost:90001/occassionsreminder/add',
-    OCCASSIONS_SERVICE_EDIT_OCCASSION_URL: 'http://localhost:9001/occassionsreminder/edit',
-    OCCASSIONS_SERVICE_DELETE_OCCASSION_URL: 'http://localhost:9001/occassionsreminder/delete',
-    REGISTER_COMPONENT_REGISTER_URL: 'http://localhost:9001/occassionsreminder/register',
-    UPDATE_COMPONENT_GET_USER_URL: 'http://localhost:9001/occassionsreminder/getUser',
-    UPDATE_COMPONENT_EDIT_USER_URL: 'http://localhost:9001/occassionsreminder/editUser'
+    OCCASSIONS_SERVICE_GET_OCCASSIONS_URL: 'http://localhost:9000/occassionsreminder/occassions',
+    OCCASSIONS_SERVICE_GET_ONE_OCCASSION_URL: 'http://localhost:9000/occassionsreminder/occassion',
+    OCCASSIONS_SERVICE_ADD_OCCASSION_URL: 'http://localhost:90000/occassionsreminder/add',
+    OCCASSIONS_SERVICE_EDIT_OCCASSION_URL: 'http://localhost:9000/occassionsreminder/edit',
+    OCCASSIONS_SERVICE_DELETE_OCCASSION_URL: 'http://localhost:9000/occassionsreminder/delete',
+    REGISTER_COMPONENT_REGISTER_URL: 'http://localhost:9000/occassionsreminder/register',
+    UPDATE_COMPONENT_GET_USER_URL: 'http://localhost:9000/occassionsreminder/getUser',
+    UPDATE_COMPONENT_EDIT_USER_URL: 'http://localhost:9000/occassionsreminder/editUser'
 
 };
 
